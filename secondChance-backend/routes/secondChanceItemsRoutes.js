b9156f0fa29d7fe1d89d745f4424ba5540779111
Sklup55 <85282385+Sklup55@users.noreply.github.com>
2024-03-22 17:55:30 +0530

    Update secondChanceItemsRoutes.js

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-const router = express.Router();
-const connectToDatabase = require('../models/db');
-const logger = require('../logger');
+const express = require('express')
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
+const router = express.Router()
+const connectToDatabase = require('../models/db')
+const logger = require('../logger')
 
 // Define the upload directory path
 const directoryPath = 'public/images';
